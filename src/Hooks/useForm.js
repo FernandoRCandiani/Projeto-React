6cb804286f01aba34e6abbc9aa1ba81aa186d6dd
Fernando <79520984+FernandoRCandiani@users.noreply.github.com>
2022-05-02 16:05:56 -0300

    Revert "Create registration page"

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,10 +6,6 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido",
   },
-  password: {
-    regex: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/,
-    message: 'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimu 8 caractereres.',
-  }
 };
 
 const useForm = (type) => {
