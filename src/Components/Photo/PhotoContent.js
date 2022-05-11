92cc30fab309bdd1ad9b3a3f4ffeb3290e9c2711
FernandoRCandiani <candianirfernando@gmail.com>
2022-05-11 14:37:33 -0300

    Adicionado função deletar

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import PhotoComments from './PhotoComments';
-import styles from './PhotoContent.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import PhotoComments from "./PhotoComments";
+import styles from "./PhotoContent.module.css";
+import PhotoDelete from "./PhotoDelete";
 
-const PhotoContent = ({data}) => {
-  const {photo, comments} = data;
+const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
+  const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
@@ -13,10 +17,14 @@ const PhotoContent = ({data}) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
-          <h1 className='title'>
+          <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
           <ul className={styles.attributes}>
@@ -27,7 +35,7 @@ const PhotoContent = ({data}) => {
       </div>
       <PhotoComments id={photo.id} comments={comments} />
     </div>
-  )
-}
+  );
+};
 
-export default PhotoContent
+export default PhotoContent;
