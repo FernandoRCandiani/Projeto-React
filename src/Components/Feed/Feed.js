f82454ce9dca167584497a3ff84dfbc4d23558f5
FernandoRCandiani <candianirfernando@gmail.com>
2022-05-16 11:59:35 -0300

    Página das foto

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -4,14 +4,14 @@ import FeedPhotos from "./FeedPhotos";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
-  const [pages, setPages] = React.useState([1, 2]);
+  const [pages, setPages] = React.useState([1]);
   const [infinite, setInfinite] = React.useState(true);
 
   React.useEffect(() => {
     let wait = false;
     function infiniteScroll() {
       if (infinite) {
-        const scroll = window.schollY;
+        const scroll = window.scrollY;
         const height = document.body.offsetHeight - window.innerHeight;
 
         if (scroll > height * 0.75 && !wait) {
--- src/Components/Helper/Image.js
+++ src/Components/Helper/Image.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './Image.module.css';
+import React from "react";
+import styles from "./Image.module.css";
 
 const Image = ({ alt, ...props }) => {
   const [skeleton, setSkeleton] = React.useState(true);
